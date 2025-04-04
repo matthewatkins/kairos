41b4e5f82feb2206a76dc990c1907e6a892cad28
Matthew Atkins <matthew@atkins.codes>
2025-04-04 11:54:00 -0500

    adding variants to panel component

---
+++ src/components/Panel.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+type Props = {
+  color?: 'energy-high' | 'energy-medium' | 'energy-low' | 'default';
+};
+
+const { color = 'default' } = defineProps<Props>();
+
+const variants = {
+  'energy-high': 'bg-energy-high',
+  'energy-medium': 'bg-energy-medium',
+  'energy-low': 'bg-energy-low',
+  default: 'bg-white',
+};
+</script>
+
+<template>
+  <section class="panel p-4 shadow-sm" :class="variants[color]">
+    <slot />
+  </section>
+</template>
