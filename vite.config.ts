bbbe1efa7e3cad5cc1b6f95ec3e7510109a3599a
Matthew Atkins <matthew@atkins.codes>
2025-05-21 16:46:55 -0500

    Remove Tailwind CSS Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import vueDevTools from 'vite-plugin-vue-devtools';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
